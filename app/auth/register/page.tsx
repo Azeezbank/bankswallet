c1b301e32d78043edacc36ab3eea7cd7ff6fc938
Azeezbank <bankoleazeezb98@gmail.com>
2026-04-25 15:41:37 +0100

    updated

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,9 +1,14 @@
+"use client";
+
+import { Suspense } from "react";
 import RegisterForm from "@/app/components/auth/register";
 
 export default function RegisterPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-app-gradient px-4">
+      <Suspense fallback={<div>Loading...</div>}>
       <RegisterForm />
+      </Suspense>
     </div>
   );
 }
